145412dd89ccabbcb437a84a25dd719dcce23f8e
Takumi Sakakibara <sakataku1104@gmail.com>
2026-02-03 11:27:53 +0900

    Add script to run MagicPod tests

---
+++ run_magicpod_test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+
+OS=mac
+FILENAME=magicpod-api-client
+
+curl -L "https://app.magicpod.com/api/v1.0/magicpod-clients/api/${OS}/latest/" -H "Authorization: Token ${MAGICPOD_API_TOKEN}" --output ${FILENAME}.zip
+unzip -q ${FILENAME}.zip
+
+export MAGICPOD_ORGANIZATION=MagicPod_Sakakibara
+export MAGICPOD_PROJECT=hands-on
+
+TEST_SETTING_NUMBER=1
+
+./magicpod-api-client batch-run -S ${TEST_SETTING_NUMBER}
+./magicpod-api-client get-batch-runs
